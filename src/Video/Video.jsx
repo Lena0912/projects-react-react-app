a6464b8aa63f9b388d8a49b990e4d53fefd91967
Lena0912 <elenabelichenko9@gmail.com>
2024-08-30 02:35:48 +0300

    update

--- src/Video/Video.jsx
+++ src/Video/Video.jsx
@@ -5,15 +5,16 @@ function Video(props) {
         const { title, channelName, img } = props;
        
         const [likesCount, setLikesCount] = useState(0)
+
         const increaseLikesByOne = () => {
-              setLikesCount(likesCount + 1) 
+              setLikesCount(prevCount => prevCount + 1) 
                 console.log(title, likesCount);
           
         };
     return (<div className='video'>
-            <img className='video-img' src={img} alt="video image" />
-            <p>{title}</p>
-            <p>{channelName}</p>
+            <img className='video-img' src={img} alt={ `${title} thumbnail`}/>
+            <p className='video-title'>{title}</p>
+            <p className='video-channel'>{channelName}</p>
             <div className='video-footer'>
               <p>Likes: {likesCount}</p>
               <button className='video-btn' onClick={increaseLikesByOne}>Likes</button>
